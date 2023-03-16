b0f976d96b8fdc1c2d76cb8a4e99428176960614
Sean Kipinä <paintballsean@gmail.com>
2023-03-16 21:27:36 +0200

    Added received function parameters and declared a scoreArray.

--- index.js
+++ index.js
@@ -164,8 +164,8 @@ Use the scoreboard function below to do the following:
   "This game will require extra innings: Away 10 - Home 10"
 ] */
 // NOTE: There is no test associated with this code; if your output matches the given example, consider it complete!
-function scoreboard(/* CODE HERE */) {
-  /* CODE HERE */
+function scoreboard(getInningScoreCallback, inningCallback, numInnings) {
+  ket scoreArray = [];
 }
 
 
